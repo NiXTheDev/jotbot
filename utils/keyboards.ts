73cdc073b5546dc64754b634dfde723586921434
codecanna <codecannamw@gmail.com>
2025-12-11 02:09:27 -0700

    added a main custom keyboard.

--- utils/keyboards.ts
+++ utils/keyboards.ts
@@ -1,4 +1,5 @@
 import { InlineKeyboard, Keyboard } from "grammy";
+import { Key } from "node:readline";
 
 export const registerKeyboard = new InlineKeyboard().text(
   "Register",
@@ -8,3 +9,16 @@ export const registerKeyboard = new InlineKeyboard().text(
 export const deleteAccountConfirmKeyboard: InlineKeyboard = new InlineKeyboard()
   .text("Yes", "delete-account-yes")
   .text("No", "delete-account-no");
+
+export const viewEntriesKeyboard: InlineKeyboard = new InlineKeyboard()
+  .text("Prev", "previous-entry")
+  .text("Delete", "delete-entry")
+  .text("Next", "next-entry").row()
+  .text("Back", "view-entry-backbutton");
+
+export const mainCustomKeyboard: Keyboard = new Keyboard()
+  .text("/new_entry").row()
+  .text("/view_entries").row()
+  .text("/delete_account")
+  .resized()
+  .persistent();

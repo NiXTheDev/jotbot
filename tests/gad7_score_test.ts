23276fbcba4c35ed7f7db00787e94f12edc46389
codecanna <codecannamw@gmail.com>
2025-12-21 03:38:51 -0700

    Started tests for gad7_score.ts

---
+++ tests/gad7_score_test.ts
@@ -0,0 +1,35 @@
+import { assertEquals, assertExists, assertNotEquals } from "@std/assert";
+import { insertGadScore } from "../models/gad7_score.ts";
+import { AnxietySeverity, GAD7Score, User } from "../types/types.ts";
+import { createGadScoreTable, createUserTable } from "../db/migration.ts";
+import { insertUser } from "../models/user.ts";
+import { testDbFile } from "../constants/paths.ts";
+// Create test user
+const testUser: User = {
+  telegramId: 12345,
+  username: "username",
+  dob: new Date(),
+  joinedDate: new Date(),
+};
+
+// Test gad7 score
+const testGadScore: GAD7Score = {
+  userId: testUser.telegramId,
+  timestamp: 12345,
+  score: 10,
+  severity: AnxietySeverity.MINIMAL_ANXIETY,
+  impactQuestionAnswer: "Test",
+};
+
+Deno.test("Test insertGadScore()", () => {
+  // Create test Database
+  createGadScoreTable(testDbFile);
+  createUserTable(testDbFile);
+  insertUser(testUser, testDbFile);
+
+  const result = insertGadScore(testGadScore, testDbFile);
+  assertEquals(result?.changes, 1);
+
+  // Clean up test db
+  Deno.remove(testDbFile);
+});

d991d0cf61bd3f19c03794b9124fb0f2efc54b33
codecanna <codecannamw@gmail.com>
2025-12-21 03:52:40 -0700

    Updated file paths.

--- constants/paths.ts
+++ constants/paths.ts
@@ -1,3 +1,3 @@
-export const dbFile: string = `${Deno.cwd()}/db/prod_db/jotbot.db`;
-export const testDbFile: string = `${Deno.cwd()}/db/test_db/jotbot_test.db`;
+export const dbFile: string = `db/prod_db/jotbot.db`;
+export const testDbFile: string = `db/test_db/jotbot_test.db`;
 export const selfieDirPath: string = `${Deno.cwd()}/assets/selfies`;

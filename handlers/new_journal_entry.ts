b019a01797dfe73c7f390d99214eca837257820e
codecanna <codecannamw@gmail.com>
2025-12-22 14:27:43 -0700

    Still figuring this out.

--- handlers/new_journal_entry.ts
+++ handlers/new_journal_entry.ts
@@ -1,11 +1,22 @@
 import { Conversation } from "@grammyjs/conversations";
 import { Context } from "grammy";
+import { JournalEntry } from "../types/types.ts";
 
 /**
  * Starts the process of creating a new journal entry.
- * @param conversation 
- * @param ctx 
+ * @param conversation Conversation
+ * @param ctx Context
  */
 export async function new_journal_entry(conversation: Conversation, ctx: Context) {
-    await ctx.reply("Start new journal entry.");
+    const ctxMsg = await ctx.reply("Tell me what's on your mind.", );
+
+    const journalEntryContent = conversation.waitFor(["message:text", "message:photo"]);
+
+    // const entry: JournalEntry = {
+    //     userId: ctx.from?.id!,
+    //     timestamp: new Date(await conversation.external(() => Date.now())),
+    //     content: (await journalEntryContent).message.text!,
+    //     length: (await journalEntryContent).message.text!.length,
+    //     images: // TODO: I need to download files and then store the paths
+    // }
 }

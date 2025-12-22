52b87dd941d080b1778951ba47611a9241481d9d
codecanna <codecannamw@gmail.com>
2025-12-22 14:27:08 -0700

    Added sql file path

--- constants/paths.ts
+++ constants/paths.ts
@@ -1,3 +1,4 @@
 export const dbFile: string = `db/prod_db/jotbot.db`;
 export const testDbFile: string = `db/test_db/jotbot_test.db`;
 export const selfieDirPath: string = `${Deno.cwd()}/assets/selfies`;
+export const sqlFilePath = "db/sql";

283b5bd18feb98c462ae8ef22d215351a1717eb3
codecanna <codecannamw@gmail.com>
2025-12-22 15:46:19 -0700

    added doc comments

--- models/phq9_score.ts
+++ models/phq9_score.ts
@@ -4,10 +4,9 @@ import { DatabaseSync } from "node:sqlite";
 import { depressionSeverityStringToEnum } from "../utils/misc.ts";
 
 /**
- * 
- * @param phqScore 
- * @param dbFile 
- * @returns 
+ * @param phqScore
+ * @param dbFile
+ * @returns
  */
 export function insertPhqScore(phqScore: PHQ9Score, dbFile: PathLike) {
   try {
@@ -37,10 +36,9 @@ export function insertPhqScore(phqScore: PHQ9Score, dbFile: PathLike) {
 }
 
 /**
- * 
- * @param userId 
- * @param dbFile 
- * @returns 
+ * @param userId
+ * @param dbFile
+ * @returns
  */
 export function getPhqScoreById(userId: number, dbFile: PathLike) {
   try {

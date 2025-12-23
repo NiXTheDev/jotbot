614bb129a089ca09b212e96bfb1e43a5d5f91bfa
codecanna <codecannamw@gmail.com>
2025-12-23 05:04:14 -0700

    Added a trigger to create user settings column when a new user is created.

--- db/sql/create_user_table.sql
+++ db/sql/create_user_table.sql
@@ -6,3 +6,8 @@ CREATE TABLE IF NOT EXISTS user_db (
     dob INTEGER NOT NULL,
     joinedDate INTEGER NOT NULL
 );
+
+-- Create user settings when a new user is created
+CREATE TRIGGER insert_user_settings AFTER INSERT ON user_db BEGIN
+    INSERT INTO settings_db (userId) VALUES (telegramId);
+END;

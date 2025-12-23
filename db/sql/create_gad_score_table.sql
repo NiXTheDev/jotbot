430a208265a8a82a9417e87f83c5fe98d8a2f12e
codecanna <codecannamw@gmail.com>
2025-12-23 05:51:56 -0700

    Fixed SQL issues

--- db/sql/create_gad_score_table.sql
+++ db/sql/create_gad_score_table.sql
@@ -1,10 +1,11 @@
 -- GAD-7 Score Table
 CREATE TABLE IF NOT EXISTS gad_score_db (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId INTEGER UNIQUE,
+    userId INTEGER NOT NULL,
     timestamp INTEGER NOT NULL,
     score INTEGER NOT NULL,
     severity TEXT NOT NULL,
-    impact TEXT NOT NULL,
+    action TEXT NOT NULL,
+    impactQuestionAnswer TEXT NOT NULL,
     FOREIGN KEY (userId) REFERENCES user_db(telegramId) ON DELETE CASCADE
 );

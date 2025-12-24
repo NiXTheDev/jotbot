37ceafbe1ee35527f323d03aa5d099ebf7984bc4
codecanna <codecannamw@gmail.com>
2025-12-24 05:49:55 -0700

    got rid of un-needed fields

--- db/sql/create_photo_table.sql
+++ db/sql/create_photo_table.sql
@@ -1,11 +1,9 @@
 -- Photo Table
 CREATE TABLE IF NOT EXISTS photo_db (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entryId INTEGER NOT NULL,
+    entryId INTEGER NOT NULL, -- Entry ID that photo is attatched to
     path TEXT NOT NULL,
-    caption TEXT NOT NULL,
-    width INTEGER NOT NULL,
-    height INTEGER NOT NULL,
+    caption TEXT,
     fileSize INTEGER NOT NULL,
     FOREIGN KEY (entryId) REFERENCES journal_db(id) ON DELETE CASCADE
 );
